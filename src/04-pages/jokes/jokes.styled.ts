fce38c1775b22fd2bd5415426fe207671c5aa836
zharinov-nikita <zharinov.nicita@gmail.com>
2023-08-31 13:39:32 +0700

    #4 - Optimized the grid for PageJokes

--- src/04-pages/jokes/jokes.styled.ts
+++ src/04-pages/jokes/jokes.styled.ts
@@ -17,11 +17,12 @@ export namespace S {
 
         @media (min-width: 576px) {
             margin-top: 128px;
+            margin-bottom: 16px;
             padding: 0px 16px;
         }
     `
 
-    export const SearchJokes = styled.div`
+    export const FindJoke = styled.div`
         max-width: 626px;
         width: 100%;
         margin: 0px auto;
@@ -30,9 +31,9 @@ export namespace S {
         gap: 12px;
     `
 
-    export const SearchJokesInput = styled(SharedUiInputComponent)``
+    export const JokeSearchField = styled(SharedUiInputComponent)``
 
-    export const FoundJokes = styled.p`
+    export const FoundJokesNumber = styled.p`
         color: ${({ theme }) => theme.colors.secondary};
         font-family: ${({ theme }) => theme.fonts.secondary};
         font-size: 16px;
@@ -40,59 +41,37 @@ export namespace S {
         font-weight: 400;
     `
 
-    export const Jokes = styled.div`
+    export const PrimaryJokesList = styled.div`
         display: grid;
-        grid-template-columns: 1fr;
         gap: 24px;
+        grid-template-columns: 1fr;
 
-        @media (min-width: 768px) and (max-width: 991.98px) {
+        @media (min-width: 726px) {
             grid-template-columns: repeat(2, 1fr);
         }
+    `
 
-        @media (min-width: 1200px) {
+    export const SecondaryJokesList = styled(PrimaryJokesList)`
+        @media (min-width: 992px) {
             grid-template-columns: repeat(3, 1fr);
         }
     `
 
-    export const FirstRowJokes = styled(Jokes)`
-        grid-template-columns: repeat(2, 1fr);
-
-        @media (max-width: 575.98px) {
-            grid-template-columns: 1fr;
-        }
-
-        @media (min-width: 576px) and (max-width: 767.98px) {
-            grid-template-columns: repeat(2, 1fr);
-        }
-
-        @media (min-width: 768px) and (max-width: 991.98px) {
-            grid-template-columns: repeat(2, 1fr);
-        }
-
-        @media (min-width: 768px) and (max-width: 991.98px) {
-            grid-template-columns: repeat(2, 1fr);
-        }
-
-        @media (min-width: 1200px) {
-            grid-template-columns: repeat(2, 1fr);
-        }
-    `
-
-    export const Joke = styled(SharedUiCardComponent)`
+    export const JokeItem = styled(SharedUiCardComponent)`
         width: 100%;
         min-height: 300px;
     `
 
-    export const JokesWrapperSkeleton = styled.div`
+    export const JokesListSkeleton = styled.div`
         display: flex;
         flex-direction: column;
         gap: 24px;
     `
 
-    // Skeleton
-    export const JokesSkeleton = styled(Jokes)``
-    export const FirstRowJokesSkeleton = styled(FirstRowJokes)``
-    export const JokeSkeleton = styled(SharedUiCardSkeleton)`
+    export const PrimaryJokesListSkeleton = styled(PrimaryJokesList)``
+    export const SecondaryJokesListSkeleton = styled(SecondaryJokesList)``
+
+    export const JokeItemSkeleton = styled(SharedUiCardSkeleton)`
         width: 100%;
         min-height: 300px;
     `
